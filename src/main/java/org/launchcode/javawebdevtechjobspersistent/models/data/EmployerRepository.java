a19d0af1b7b5e4621f4c99b7940acf32a1dea143
Camila <camila@Matthews-MacBook-Pro-2.local>
2021-03-30 13:26:47 -0500

    rename the repositories to follow naming convention

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
@@ -2,5 +2,5 @@ package org.launchcode.javawebdevtechjobspersistent.models.data;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface Employer extends CrudRepository<Employer, Integer> {
+public interface EmployerRepository extends CrudRepository<EmployerRepository, Integer> {
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
@@ -2,5 +2,5 @@ package org.launchcode.javawebdevtechjobspersistent.models.data;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface Skill extends CrudRepository<Skill, Integer> {
+public interface SkillRepository extends CrudRepository<SkillRepository, Integer> {
 }
